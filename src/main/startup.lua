1929a5a36fb6e022b4a0b3ed989a6fe0c105055b
Lord-Cuddles <georgeburton666@hotmail.com>
2020-05-29 19:32:03 +0100

    startup has been updated
    Changes priority of file checking

--- src/main/startup.lua
+++ src/main/startup.lua
@@ -1,5 +1,31 @@
-if fs.exists("quiz.lua") then
-    shell.run("quiz.lua")
-else
+if not fs.exists("update.lua") then
+    -- Download and install the updater silently
+    if http.checkURL(
+        "https://raw.githubusercontent.com/Lord-Cuddles/Quiz/master/src/main/update.lua"
+    ) == false then
+        print("Error: Cannot connect to GitHub")
+    return end
+    
+    local response, err = http.get("https://raw.githubusercontent.com/Lord-Cuddles/Quiz/master/src/main/update.lua")
+    if response then
+        local content = response.readAll()
+        response.close()
+        
+        local file = fs.open("update.lua", "w")
+        file.write(content)
+        file.close()
+        
+        shell.run("update.lua")
+    else
+        print("Error: Cannot retrieve from GitHub")
+    end
+        
+elseif not fs.exists("quiz.lua") then
+    -- Run the updater to get the installer
     shell.run("update.lua")
+    
+else
+    -- Run the main quiz file
+    shell.run("quiz.lua")
+
 end
