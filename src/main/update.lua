28933cee5be765b23ac65f10315e18fd2d02bd11
Lord-Cuddles <georgeburton666@hotmail.com>
2020-05-29 17:17:17 +0100

    Update update.lua

--- src/main/update.lua
+++ src/main/update.lua
@@ -1,3 +1,4 @@
+
 -- Error handling for checking 
 if http.checkURL(
     "https://raw.githubusercontent.com/Lord-Cuddles/Quiz/master/src/main/quiz.lua"
@@ -6,7 +7,7 @@ if http.checkURL(
     return
 end
 
-
+-- Download url here
 local response, err = http.get(
     "https://raw.githubusercontent.com/Lord-Cuddles/Quiz/master/src/main/quiz.lua"
 )
@@ -20,6 +21,13 @@ if response then
     local file = fs.open("download.lua", "w")
     file.write(content)
     file.close()
+    if fs.exists("quiz.lua") then
+        local file = fs.open("quiz.lua", "r")
+        if file.readAll() == content then
+            print("Warning: No changes between versions, possibly discord cache")
+            os.pullEvent("char")
+        end
+    end
     
     if fs.exists("old_quiz.lua") then
         fs.delete("old_quiz.lua")
@@ -28,8 +36,18 @@ if response then
         fs.move("quiz.lua", "old_quiz.lua")
     end
     if fs.exists("download.lua") then
+        term.setTextColor(colours.green)
+        print("Installation completed successfully!")
         fs.move("download.lua", "quiz.lua")
+    else
+        term.setTextColor(colours.red)
+        print("Installation failed to complete!")
+        fs.move("old_quiz.lua", "quiz.lua")
     end
+    
+    term.setTextColor(colours.grey)
+    print("Press <any key> to continue")
+    os.pullEvent("key")
     
     shell.run("quiz.lua")
     
