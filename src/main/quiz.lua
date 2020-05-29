583eb37ca835a16e164b0f69cc336d621902818c
Lord-Cuddles <georgeburton666@hotmail.com>
2020-05-29 16:57:28 +0100

    Fix 1.0 alpha 20 again

--- src/main/quiz.lua
+++ src/main/quiz.lua
@@ -133,9 +133,6 @@ function getQwithA(t)
 end
 
 function addLog(logtable, question, answer, correct)
-    if not type(logtable) == "table" then
-        error("Missing a log table") 
-    end
     logtable[#logtab1e] = question.."|"..answer.."|"..tostring(correct)
 end
 
