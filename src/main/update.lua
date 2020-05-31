3e987e3d77ace48f3008baf7b57728bf0da80bac
Lord-Cuddles <georgeburton666@hotmail.com>
2020-05-31 16:39:51 +0100

    Update 1.2
    Migrated to themes table from hardcoded colours to make it easier to change colours in the future

--- src/main/update.lua
+++ src/main/update.lua
@@ -1,6 +1,27 @@
 
-updater_version = "1.1.1"
+updater_version = "1.2"
 args = {...}
+theme = {
+    bg=colours.black,
+    sel=colours.green,
+    sel_done=colours.red,
+    desel=colours.white,
+    desel_done=colours.grey,
+    head_bg=colours.orange,
+    head_fg=colours.black,
+    warn_bg=colours.red,
+    foot=colours.grey,
+    foot_note1=colours.cyan,
+    foot_note2=colours.magenta,
+    right=colours.green,
+    wrong=colours.red,
+    decor_1=colours.grey,
+    decor_2=colours.lightGrey,
+    text=colours.white,
+    title=colours.orange,
+    unanswered=colours.yellow,
+    points=colours.yellow
+}
 term.clear()
 
 url = "quiz.lua"
@@ -12,7 +33,6 @@ elseif args[1] == "startup" then
     url = "startup.lua"
     dir = "startup"
 end
-term.setTextColor(colours.lightGrey)
 
 xSize, ySize = term.getSize()
 
@@ -25,13 +45,15 @@ function midPrint(text, writeInstead)
 end
 
 term.setCursorPos(1,1)
-term.setBackgroundColor(colours.aqua or colours.cyan)
-term.setTextColor(colours.white)
+term.setBackgroundColor(theme.head_bg)
+term.setTextColor(theme.head_fg)
 term.clearLine()
-midPrint("Quizzie Installer: "..updater_version)
-term.setBackgroundColor(colours.black)
-term.setTextColor(colours.green)
+midPrint("Installer: "..updater_version)
+term.setBackgroundColor(theme.bg)
 print()
+term.setTextColor(theme.decor_1)
+midPrint("*  *  *  *  *  *  *  *  *  *")
+term.setTextColor(theme.sel)
 
 midPrint("Attempting download of "..dir)
 
@@ -39,7 +61,7 @@ midPrint("Attempting download of "..dir)
 if http.checkURL(
     "https://raw.githubusercontent.com/Lord-Cuddles/Quiz/master/src/main/"..url
 ) == false then
-    term.setTextColor(colours.red)
+    term.setTextColor(theme.wrong)
     print()
     midPrint("Unable to connect to GitHub")
     return
@@ -62,14 +84,14 @@ if response then
     if fs.exists(dir) then
         local file = fs.open(dir, "r")
         if file.readAll() == content then
-            term.setTextColor(colours.yellow)
+            term.setTextColor(theme.foot_note1)
             print()
             midPrint("Note: No changes between versions")
-            term.setTextColor(colours.lightGrey)
-            midPrint("Could be a caching issue, try later")
+            term.setTextColor(theme.decor_2)
+            midPrint("Probably a caching issue, try in 5 mins")
         end
     end
-    term.setTextColor(colours.grey)
+    term.setTextColor(theme.foot)
     if fs.exists("old_quiz.lua") and dir == "quiz.lua" then
         print()
         midPrint("Starting backup of old quiz file...")
@@ -84,16 +106,16 @@ if response then
     end
     print()
     if fs.exists("download.lua") then
-        term.setTextColor(colours.green)
+        term.setTextColor(theme.right)
         midPrint("Installation completed successfully!")
         fs.move("download.lua", dir)
     else
-        term.setTextColor(colours.red)
+        term.setTextColor(theme.wrong)
         print("Installation failed to install!")
         fs.move("old_quiz.lua", dir)
     end
     
-    term.setTextColor(colours.grey)
+    term.setTextColor(theme.foot_note2)
     term.setCursorPos(1,ySize)
     midPrint("Press <any key> to continue", true)
     os.pullEvent("key")
@@ -102,6 +124,7 @@ if response then
     
 else
     
+    term.setTextColor(theme.wrong)
     print("Failed to get a response")
     
 end
